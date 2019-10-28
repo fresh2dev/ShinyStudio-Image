05235b5e074c6a470d12fd1b9858da1c73e7d645
donald <dm3ll3n@gmail.com>
2019-10-27 22:35:22 -0500

    updated render-notebook.ps1

--- configs/cronicle/jobs/render-notebook.ps1
+++ configs/cronicle/jobs/render-notebook.ps1
@@ -3,14 +3,18 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$Source,
     [string]$Destination,
-    [string]$To = 'html',
-    [switch]$Execute
+    [string]$To = 'html'
 )
+
+$Source = $Source | Resolve-Path
 
 [string]$SourceDir  = Split-Path $Source -Parent
 
-if (-not $SourceDir) {
-    $SourceDir = Resolve-Path '.'
+if (-not $Destination) {
+    $Destination = Join-Path $SourceDir 'index.html'
+}
+else {
+    $Destination = $Destination | Resolve-Path
 }
 
 [string]$SourceFile = Split-Path $Source -Leaf
@@ -18,9 +22,6 @@ if (-not $SourceDir) {
 [string]$HtmlName   = [System.IO.Path]::GetFileNameWithoutExtension($SourceFile) + '.html'
 [string]$HtmlPath   = Join-Path $SourceDir $HtmlName
 
-if (-not $Destination) {
-    $Destination = Join-Path $SourceDir 'index.html'
-}
 
 $env:PATH = "/conda3/envs/$env:VIRTUAL_ENV/bin:" + $env:PATH
 
@@ -28,7 +29,7 @@ if ($FileExt.ToLower() -eq '.rmd') {
     & /usr/local/bin/r -e "rmarkdown::render('$Source', 'html_document', output_file='$Destination')"
 }
 elseif ($FileExt.ToLower() -eq '.ipynb') {
-    & jupyter nbconvert "$Source" --to $To -y --template full
+    & jupyter nbconvert "$Source" --to $To --execute -y --template full
     Write-Host "Moving '$HtmlPath' to '$Destination'"
     Move-Item "$HtmlPath" "$Destination" -Force
 }
